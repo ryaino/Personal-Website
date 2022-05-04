b7ddc441b881362f54c719fc478ff5acc71beef2
Ryan Field <fieldryan19@gmail.com>
2022-05-05 01:24:26 +1200

    animate on view

--- javascript/technologies.js
+++ javascript/technologies.js
@@ -6,6 +6,24 @@ const technologies = {
         'Firebase', 'Angular', 'Sass', 'Typescript', 'Javascript', 'Ionic', 'Electron'
     ]
 }
+window.addEventListener('scroll', reveal);
+function reveal() {
+
+    let reveals = document.querySelectorAll('.technologies__single');
+
+    for (const iterator of reveals) {
+
+        let windowHeight = window.innerHeight;
+        let revealtop = iterator.getBoundingClientRect().top;
+
+        if(revealtop < windowHeight){
+            iterator.classList.add('active');
+        }
+    }
+}
+
+
+
 
 for (const [key, value] of Object.entries(technologies)) {
     let component = document.getElementById('icon-grid--' + key);
@@ -23,14 +41,12 @@ for (const [key, value] of Object.entries(technologies)) {
         let headerElement = document.createElement('h3');
         headerElement.textContent = name;
 
-        techElement.style.animationDelay = (index * 500) + 'ms';
+        techElement.style.animationDelay = (index * 150) + 'ms';
         techElement.appendChild(imageElement);
         techElement.appendChild(headerElement);
         component.appendChild(techElement);
         index += 1;
 
     }
-
-
-
 }
+
