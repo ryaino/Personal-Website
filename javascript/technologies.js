e150e289a2233476494b69735439f94a4dee9c07
Ryan Field <fieldryan19@gmail.com>
2022-05-04 23:27:03 +1200

    added animations

--- javascript/technologies.js
+++ javascript/technologies.js
@@ -9,7 +9,7 @@ const technologies = {
 
 for (const [key, value] of Object.entries(technologies)) {
     let component = document.getElementById('icon-grid--' + key);
-
+    let index = 1;
     for (const element of value) {
         let name = element;
 
@@ -23,9 +23,14 @@ for (const [key, value] of Object.entries(technologies)) {
         let headerElement = document.createElement('h3');
         headerElement.textContent = name;
 
+        techElement.style.animationDelay = (index * 500) + 'ms';
         techElement.appendChild(imageElement);
         techElement.appendChild(headerElement);
         component.appendChild(techElement);
+        index += 1;
 
     }
+
+
+
 }
